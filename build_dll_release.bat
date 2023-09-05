89ff0cdf3856cdd5d475f6e281a568de9a42b746
AzureAD\강범수 <bumpsgoodman@gmail.com>
2023-09-05 16:01:53 +0900

    Update: 빌드 스크립트 수정

--- build_dll_release.bat
+++ build_dll_release.bat
@@ -8,5 +8,5 @@
 "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" project\safe99_ecs\safe99_ecs\safe99_ecs.vcxproj /t:rebuild /p:configuration=release /p:platform=x64
 "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" project\safe99_ecs\safe99_ecs\safe99_ecs.vcxproj /t:rebuild /p:configuration=release /p:platform=Win32
 
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" project\safe99_renderer\safe99_renderer\safe99_renderer.vcxproj /t:rebuild /p:configuration=release /p:platform=x64
-"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" project\safe99_renderer\safe99_renderer\safe99_renderer.vcxproj /t:rebuild /p:configuration=release /p:platform=Win32
+"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" project\safe99_renderer_ddraw\safe99_renderer_ddraw\safe99_renderer_ddraw.vcxproj /t:rebuild /p:configuration=release /p:platform=x64
+"C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" project\safe99_renderer_ddraw\safe99_renderer_ddraw\safe99_renderer_ddraw.vcxproj /t:rebuild /p:configuration=release /p:platform=Win32
